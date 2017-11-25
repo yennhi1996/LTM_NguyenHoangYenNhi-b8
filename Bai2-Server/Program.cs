655b0cf978df1fbb44d535d46149f6f06cfc6036
nataurus9x <nataurus9x@gmail.com>
2017-11-25 11:04:43 +0700

    Buoi8

---
+++ Bai2-Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Bai2_Server
+{
+    class Program
+    {
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint iep = new IPEndPoint(IPAddress.Parse("224.100.0.1"), 9050);
+        byte[] data = Encoding.ASCII.GetBytes("This is a test message");
+        server.SendTo(data, iep);  
+        server.Close();
+
+        }
+    }
+}
---
+++ Broadcast/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Broadcast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint iep1 = new IPEndPoint(IPAddress.Broadcast, 9050);
+            IPEndPoint iep2 = new IPEndPoint(IPAddress.Parse("192.168.1.255"), 9050); string hostname = Dns.GetHostName();
+            byte[] data = Encoding.ASCII.GetBytes(hostname);
+            sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+            sock.SendTo(data, iep1);
+            sock.SendTo(data, iep2);
+            sock.Close();
+        }
+    }
+}
